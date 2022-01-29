0ccd1065c5a99f226573b5d6c297d484ccdf27a8
Daniil Belyakov <bds@pkzdrav.ru>
2022-01-29 13:47:15 +0500

    https://hyperskill.org/projects/47/stages/251

--- Maze Runner/task/src/maze/Main.java
+++ Maze Runner/task/src/maze/Main.java
@@ -2,6 +2,28 @@ package maze;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int[][] multi = new int[][]{
+                {1, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                {1, 0, 1, 0, 1, 1, 1, 1, 0, 1},
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 0, 1, 1, 0, 1, 1},
+                {1, 0, 1, 0, 0, 1, 0, 0, 1, 1},
+                {1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
+                {1, 0, 0, 1, 0, 1, 0, 1, 0, 1},
+                {1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        };
+        for (int[] ints : multi) {
+            for (int i : ints) {
+                if (i == 0) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("\u2588\u2588");
+                }
+            }
+            System.out.println();
+        }
     }
 }
